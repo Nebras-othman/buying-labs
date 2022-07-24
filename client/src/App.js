e00b7ae109216697419a3dec70c7d94d18de1038
Nebras Othman <nebras@showroom.app>
2022-07-24 12:49:45 +0200

    Create client folder nad create basic react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Paper, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import MapIcon from '@mui/icons-material/Map';
+import "./App.css";
+
+function App() {
+  return <div className='App'>
+  <div className="search-bar">
+      Hello world
+  </div>
+
+  </div>;
+}
+
+export default App;
